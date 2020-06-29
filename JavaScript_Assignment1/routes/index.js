2a4d853a3cafe374fe5dc9b74cda32130e8b7331
nadiarose1205 <200344428@student.georgianc.on.ca>
2020-06-29 16:46:57 -0400

    Defined routes in index.js

--- JavaScript_Assignment1/routes/index.js
+++ JavaScript_Assignment1/routes/index.js
@@ -7,4 +7,24 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+// get Projects page
+router.get('/', function (req, res) {
+    res.render('projects');
+});
+
+// get Services page
+router.get('/', function (req, res) {
+    res.render('services');
+});
+
+// get About Me page
+router.get('/', function (req, res) {
+    res.render('about_me');
+});
+
+// get Contact page
+router.get('/', function (req, res) {
+    res.render('contact_me');
+});
+
 module.exports = router;
