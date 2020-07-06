2036e9be456634b83154b2328330d3b6ece202c8
nadiarose1205 <200344428@student.georgianc.on.ca>
2020-07-06 16:23:17 -0400

    All pages running

--- JavaScript_Assignment1/routes/index.js
+++ JavaScript_Assignment1/routes/index.js
@@ -4,27 +4,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { 'title': 'Home' });
 });
 
 // get Projects page
-router.get('/', function (req, res) {
-    res.render('projects', { title: 'Projects'});
+router.get('/projects', function (req, res) {
+    res.render('projects', { 'title': 'Projects'});
 });
 
 // get Services page
-router.get('/', function (req, res) {
-    res.render('services', { title: 'Services'});
+router.get('/services', function (req, res) {
+    res.render('services', { 'title': 'Services'});
 });
 
 // get About Me page
-router.get('/', function (req, res) {
-    res.render('about_me');
+router.get('/about_me', function (req, res) {
+    res.render('about_me', { 'title': 'About Me'});
 });
 
 // get Contact page
-router.get('/', function (req, res) {
-    res.render('contact_me');
+router.get('/contact_me', function (req, res) {
+    res.render('contact_me', { 'title': 'Contact Me'});
 });
 
 module.exports = router;
